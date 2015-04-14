b1a7c1408e16a9ed8e5982ecab67f5f8f2c577f9
Loïc Delmaire <loic.delmaire@gmail.com>
2015-04-14 10:26:28 +0200

    FIX SCUMBAG SKELZ0R

--- spec/lita/handlers/fyjc_spec.rb
+++ spec/lita/handlers/fyjc_spec.rb
@@ -1,14 +1,14 @@
 require "spec_helper"
 
 describe Lita::Handlers::Fyjc, lita_handler: true do
-  it { is_expected.to route("fyjc") }
-  it { is_expected.not_to route("fyjc oki") }
-  it { is_expected.to route("  fyjc ") }
+  it { is_expected.to route("lita fyjc") }
+  it { is_expected.not_to route("lita fyjc oki") }
+  it { is_expected.to route("lita  fyjc ") }
 
   it "works" do
     images = File.readlines(File.expand_path(File.join(File.dirname(__FILE__), "../../../lib/lita/handlers/fyjc.txt")))
     images.map!{ |img| img.gsub("\n", "") }
-    send_message('fyjc')
+    send_message('lita fyjc')
 
     (images - replies).size.should == (images.length - 1)
   end
